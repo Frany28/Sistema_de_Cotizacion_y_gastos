6b265095d7ff89bd7b2b044930b95d96bd249ca9
Frany28 <franciely.vivas@gmail.com>
2025-05-15 23:55:35 -0400

    Update server.js

--- Backend/config/server.js
+++ Backend/config/server.js
@@ -40,7 +40,7 @@ const __dirname = path.dirname(__filename);
 const clientDist = path.resolve(__dirname, "../../dist");
 if (fs.existsSync(clientDist)) {
   app.use(express.static(clientDist));
-  app.get("/*", (_req, res) =>
+  app.get("/.*/", (_req, res) =>
     res.sendFile(path.join(clientDist, "index.html"))
   );
 }
