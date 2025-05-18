d7421da17503f6879e4e90f232145818b563617d
Frany28 <franciely.vivas@gmail.com>
2025-05-17 21:12:59 -0400

    Update Login.jsx

--- Frontend/src/pages/Login/Login.jsx
+++ Frontend/src/pages/Login/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
 
     try {
       const res = await axios.post(
-        "http://localhost:3000/api/auth/login",
+        `${import.meta.env.VITE_API_URL}/auth/login`,
         { email, password },
         { withCredentials: true }
       );
