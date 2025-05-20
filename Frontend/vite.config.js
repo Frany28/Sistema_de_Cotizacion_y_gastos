8b35bfd4756dc929dfb24cc08f6e8c0e024a58f3
Frany28 <franciely.vivas@gmail.com>
2025-05-19 22:32:07 -0400

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,18 +1,19 @@
-// Frontend/vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
   root: ".",
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  css: {
+    postcss: "./postcss.config.js",
+  },
   server: {
     host: "0.0.0.0",
     port: 5173,
   },
   build: {
-    outDir: "dist", 
+    outDir: "dist",
     emptyOutDir: true,
   },
   resolve: {
