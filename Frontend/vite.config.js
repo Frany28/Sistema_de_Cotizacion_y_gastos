0886795a5216b5113528b999ae4970353ccb96e3
Frany28 <franciely.vivas@gmail.com>
2025-05-19 21:52:28 -0400

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  root: ".", // ahora el config vive dentro de Frontend
+  root: ".",
   plugins: [react(), tailwindcss()],
   server: {
     host: "0.0.0.0",
