2aaf99c22f8a5c54ddeeccfef542456f027f249d
Frany28 <franciely.vivas@gmail.com>
2025-05-21 00:07:04 -0400

    Update index.js

--- Frontend/src/api/index.js
+++ Frontend/src/api/index.js
@@ -4,7 +4,7 @@ console.log("VITE_API_URL →", import.meta.env.VITE_API_URL);
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 
-  withCredentials: true, // para que viaje la cookie de sesión
+  withCredentials: true, 
 });
 
 export default api;
