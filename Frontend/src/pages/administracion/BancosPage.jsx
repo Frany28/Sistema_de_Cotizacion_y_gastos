3ecc61684261771d4aad4a385899d958f3243896
Frany28 <franciely.vivas@gmail.com>
2025-05-24 22:35:22 -0400

    Update BancosPage.jsx

--- Frontend/src/pages/administracion/BancosPage.jsx
+++ Frontend/src/pages/administracion/BancosPage.jsx
@@ -2,11 +2,14 @@ import BancosCRUD from "../../components/BancosCRUD";
 
 function BancosPage() {
   return (
-    <div>
-      <h1>Bancos Page</h1>
-      <BancosCRUD />
-      <p>Esta es la página de administración de bancos.</p>
-    </div>
+    <main className="min-h-screen bg-gray-900 py-4 px-2 sm:px-4">
+      <h1 className="text-2xl font-bold text-white mb-4">Lista de Bancos</h1>
+      <section className="max-w-7xl mx-auto w-full">
+        <div className="bg-gray-800 shadow-lg rounded-xl overflow-auto p-4 sm:p-6">
+          <BancosCRUD />
+        </div>
+      </section>
+    </main>
   );
 }
 export default BancosPage;
