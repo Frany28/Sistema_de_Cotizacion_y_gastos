9f915092d0f400b0ec2c3986f0f1de5aa695dc68
Frany28 <franciely.vivas@gmail.com>
2025-05-24 22:09:39 -0400

    Update BancosPage.jsx

--- Frontend/src/pages/administracion/BancosPage.jsx
+++ Frontend/src/pages/administracion/BancosPage.jsx
@@ -1,10 +1,10 @@
-import BancosCRUD from "../../components/administracion/BancosCRUD";
+import BancosCRUD from "../../components/BancosCRUD";
 
 function BancosPage() {
   return (
     <div>
       <h1>Bancos Page</h1>
-      <BancosCRUD></BancosCRUD>
+      <BancosCRUD />
       <p>Esta es la página de administración de bancos.</p>
     </div>
   );
