6b433fab8d7c8e9b51ef142b57838f410f64c5bb
Frany28 <franciely.vivas@gmail.com>
2025-06-12 20:25:07 -0400

    Instalacion de nueva version purppeter

--- Backend/src/controllers/cotizaciones.controller.js
+++ Backend/src/controllers/cotizaciones.controller.js
@@ -592,7 +592,7 @@ export const generarPDFCotizacion = async (req, res) => {
     const browser = await puppeteer.launch({
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath(),
+      executablePath: await chromium.executablePath,
       headless: chromium.headless,
       ignoreHTTPSErrors: true,
     });
