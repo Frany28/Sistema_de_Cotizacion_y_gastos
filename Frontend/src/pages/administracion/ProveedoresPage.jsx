2389ee59ae8967085387a39f938d9f200a327188
Frany28 <franciely.vivas@gmail.com>
2025-07-01 22:40:56 -0400

    refactor: remove unnecessary props from ListaProveedores component in ProveedoresPage

--- Frontend/src/pages/administracion/ProveedoresPage.jsx
+++ Frontend/src/pages/administracion/ProveedoresPage.jsx
@@ -8,12 +8,9 @@ function ProveedoresPage() {
       <h1 className="text-2xl font-bold text-white mb-4">
         Lista de Proveedores
       </h1>
+
       <section className="max-w-7xl mx-auto w-full">
         <div className="bg-gray-800 shadow-lg rounded-xl overflow-auto p-4 sm:p-6">
-          <ListaProveedores
-            proveedores={proveedores}
-            actualizarLista={obtenerProveedores}
-          />
           <ListaProveedores />
         </div>
       </section>
