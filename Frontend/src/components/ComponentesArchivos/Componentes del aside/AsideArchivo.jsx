b09c39f37388c889a456e0ad74e1c3063e9d24eb
Frany28 <franciely.vivas@gmail.com>
2025-07-13 22:05:06 -0400

    .

--- Frontend/src/components/ComponentesArchivos/Componentes del aside/AsideArchivo.jsx
+++ Frontend/src/components/ComponentesArchivos/Componentes del aside/AsideArchivo.jsx
@@ -5,7 +5,7 @@ export default function AsideArchivo() {
   return (
     <>
       <div className="left-0 w-[320px] h-screen bg-gray-800">
-        <div className="flex flex-col items-center gap-4 p-4 pt-25 pl-8">
+        <div className="grid justify-center items-center gap-4 pl-4 pt-25">
           <VistaPreviaAlmacenamiento />
           <PapeleraButton />
           <RegistroDeActividades />
