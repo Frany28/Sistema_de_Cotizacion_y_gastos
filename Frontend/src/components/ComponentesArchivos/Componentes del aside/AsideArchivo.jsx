3c674f846f848fbc32ab542c77627614baba7fc5
Frany28 <franciely.vivas@gmail.com>
2025-07-13 22:00:06 -0400

    .

--- Frontend/src/components/ComponentesArchivos/Componentes del aside/AsideArchivo.jsx
+++ Frontend/src/components/ComponentesArchivos/Componentes del aside/AsideArchivo.jsx
@@ -4,7 +4,7 @@ import RegistroDeActividades from "./RegistroDeActividades";
 export default function AsideArchivo() {
   return (
     <>
-      <div className="left-0 w-[320px] h-full bg-gray-800">
+      <div className="left-0 w-[320px] h-screen bg-gray-800">
         <div className="flex flex-col items-center gap-4 p-4 pt-25 pl-8">
           <VistaPreviaAlmacenamiento />
           <PapeleraButton />
