83ef9f4b5994706137ed875a2d478e0bba1b58df
Frany28 <franciely.vivas@gmail.com>
2025-07-14 18:39:52 -0400

    busqueda actualizado

--- Frontend/src/components/ComponentesArchivos/Busqueda.jsx
+++ Frontend/src/components/ComponentesArchivos/Busqueda.jsx
@@ -1,8 +1,8 @@
 function Busqueda() {
   return (
     <>
-      <div className="w-[711px] h-[83px] bg-[#1F2937FF] rounded-3xl pt-5">
-        {" "}
+      <div className="pt-5">
+        <div className="w-[711px] h-[83px] bg-[#1F2937FF] rounded-3xl p-5"></div>
       </div>
     </>
   );
