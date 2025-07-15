23b03710c8b4346d2091d6b4b29f906085a17417
Frany28 <franciely.vivas@gmail.com>
2025-07-14 22:26:50 -0400

    busqueda eliminado

--- Frontend/src/pages/Gestor de Archivos/ArchivosPage.jsx
+++ Frontend/src/pages/Gestor de Archivos/ArchivosPage.jsx
@@ -1,5 +1,4 @@
 import AsideArchivo from "../../components/ComponentesArchivos/Componentes del aside/AsideArchivo";
-import Busqueda from "../../components/ComponentesArchivos/Busqueda";
 import TablaArchivos from "../../components/ComponentesArchivos/TablaArchivos";
 
 function ArchivosPage() {
@@ -8,7 +7,6 @@ function ArchivosPage() {
       <div className="flex w-full h-full gap-10">
         <AsideArchivo />
         <div className="gap-3 flex flex-col w-full h-full">
-          <Busqueda />
           <TablaArchivos />
         </div>
       </div>
