6c8b0d691ff59b0da6a816e8a35e3b2fc73903b1
Frany28 <franciely.vivas@gmail.com>
2025-07-18 14:27:47 -0400

    cambios en  tamaño

--- Backend/src/controllers/archivos.controller.js
+++ Backend/src/controllers/archivos.controller.js
@@ -534,7 +534,7 @@ export const obtenerArbolArchivos = async (req, res) => {
   try {
     /* 1) Traer los archivos visibles para este usuario */
     const [rows] = await db.query(
-      `SELECT id, nombreOriginal, extension, tamanioBytes,
+      `SELECT id, nombreOriginal, extension, tamanoBytes,
               rutaS3, creadoEn
          FROM archivos
         WHERE estado = 'activo'
