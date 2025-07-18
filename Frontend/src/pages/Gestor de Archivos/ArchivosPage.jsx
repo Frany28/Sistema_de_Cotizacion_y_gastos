a6e3b8cbd41990084e58faf1150db97cf72e20c7
Frany28 <franciely.vivas@gmail.com>
2025-07-18 18:09:23 -0400

    mejoras en el frontend

--- Frontend/src/pages/Gestor de Archivos/ArchivosPage.jsx
+++ Frontend/src/pages/Gestor de Archivos/ArchivosPage.jsx
@@ -4,9 +4,9 @@ import TablaArchivos from "../../components/ComponentesArchivos/TablaArchivos";
 function ArchivosPage() {
   return (
     <>
-      <div className="flex w-full h-full gap-10">
+      <div className="flex w-full h-full gap-4">
         <AsideArchivo />
-        <div className="gap-3 flex flex-col w-full h-full">
+        <div className="gap-3 flex flex-col w-full h-full p-4">
           <TablaArchivos />
         </div>
       </div>
