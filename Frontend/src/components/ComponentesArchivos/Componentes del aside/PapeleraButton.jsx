37aa9282bbf6e47612617a77c2c13a712943525c
Frany28 <franciely.vivas@gmail.com>
2025-07-21 15:29:01 -0400

    Refactor PapeleraButton component: update navigation method, enhance styling, and improve conditional rendering

--- Frontend/src/components/ComponentesArchivos/Componentes del aside/PapeleraButton.jsx
+++ Frontend/src/components/ComponentesArchivos/Componentes del aside/PapeleraButton.jsx
@@ -1,18 +1,30 @@
+// PapeleraButton.jsx
+import { useLocation, useNavigate } from "react-router-dom";
 import { Trash2 } from "lucide-react";
 
 function PapeleraButton() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const estaEnPapelera = location.pathname === "/papelera";
+
   return (
-    <>
-      <div
-        className="w-69 h-30 bg-gray-700 rounded-2xl"
-        onClick={() => (window.location.href = "/papelera")}
-      >
-        <div className="flex flex-col items-center justify-center h-full gap-2 hover:bg-gray-400 hover:cursor-pointer transition-colors duration-300 rounded-2xl">
-          <Trash2 size={40} color="#1A56DBFF" />
-          <p className="text-white font-semibold">Papelera</p>
-        </div>
+    <div
+      onClick={() => navigate("/papelera")}
+      className={`w-69 h-30 rounded-2xl cursor-pointer transition-colors duration-300 ${
+        estaEnPapelera ? "bg-blue-700" : "bg-gray-700 hover:bg-gray-500"
+      }`}
+    >
+      <div className="flex flex-col items-center justify-center h-full gap-2">
+        <Trash2 size={40} color={estaEnPapelera ? "#FFFFFF" : "#1A56DBFF"} />
+        <p
+          className={`font-semibold ${
+            estaEnPapelera ? "text-white" : "text-gray-200"
+          }`}
+        >
+          Papelera
+        </p>
       </div>
-    </>
+    </div>
   );
 }
 
