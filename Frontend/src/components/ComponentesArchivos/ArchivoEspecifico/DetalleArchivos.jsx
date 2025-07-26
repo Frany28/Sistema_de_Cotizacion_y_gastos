2b1b3e487d2c662c01cd9b92a7171b300bd81b03
Frany28 <franciely.vivas@gmail.com>
2025-07-26 13:44:01 -0400

    cambio en cursor

--- Frontend/src/components/ComponentesArchivos/ArchivoEspecifico/DetalleArchivos.jsx
+++ Frontend/src/components/ComponentesArchivos/ArchivoEspecifico/DetalleArchivos.jsx
@@ -111,7 +111,7 @@ const DetalleArchivo = () => {
       <div className="w-full">
         <button
           onClick={() => navigate(-1)}
-          className="mb-6 flex items-center gap-2 text-gray-400 hover:text-white transition-colors"
+          className="mb-6 flex items-center gap-2 text-gray-400 hover:text-white transition-colors cursor-pointer"
         >
           <ArrowLeft className="w-5 h-5" />
           Volver
--- Frontend/src/pages/Papelera/Papelera.jsx
+++ Frontend/src/pages/Papelera/Papelera.jsx
@@ -9,7 +9,7 @@ function Papelera() {
     <>
       <button
         onClick={() => navigate(-1)}
-        className="mb-6 flex items-center gap-2 text-gray-400 hover:text-white transition-colors"
+        className="mb-6 flex items-center gap-2 text-gray-400 hover:text-white transition-colors cursor-pointer"
       >
         <ArrowLeft className="w-5 h-5" />
         Volver
