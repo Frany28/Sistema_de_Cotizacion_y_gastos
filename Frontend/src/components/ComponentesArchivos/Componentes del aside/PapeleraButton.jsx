522a155b4d02d0c49bf7bb1658e73ce66d585f7f
Frany28 <franciely.vivas@gmail.com>
2025-07-28 16:00:07 -0400

    .

--- Frontend/src/components/ComponentesArchivos/Componentes del aside/PapeleraButton.jsx
+++ Frontend/src/components/ComponentesArchivos/Componentes del aside/PapeleraButton.jsx
@@ -10,23 +10,20 @@ function PapeleraButton() {
   return (
     <div
       onClick={() => navigate("/papelera")}
-      className={`w-full sm:w-44 lg:w-48 h-24 sm:h-24 rounded-2xl cursor-pointer transition-colors duration-300 flex flex-col items-center justify-center ${
-        estaEnPapelera ? "bg-blue-700" : "bg-gray-700 hover:bg-gray-600"
+      className={`w-69 h-30 rounded-2xl cursor-pointer transition-colors duration-300 ${
+        estaEnPapelera ? "bg-blue-700" : "bg-gray-700 hover:bg-gray-500"
       }`}
     >
-      <Trash2
-        size={28}
-        className={`transition duration-200 ${
-          estaEnPapelera ? "text-white" : "text-blue-400"
-        }`}
-      />
-      <p
-        className={`mt-2 text-sm font-semibold ${
-          estaEnPapelera ? "text-white" : "text-gray-200"
-        }`}
-      >
-        Papelera
-      </p>
+      <div className="flex flex-col items-center justify-center h-full gap-2">
+        <Trash2 size={40} color={estaEnPapelera ? "#FFFFFF" : "#1A56DBFF"} />
+        <p
+          className={`font-semibold ${
+            estaEnPapelera ? "text-white" : "text-gray-200"
+          }`}
+        >
+          Papelera
+        </p>
+      </div>
     </div>
   );
 }
