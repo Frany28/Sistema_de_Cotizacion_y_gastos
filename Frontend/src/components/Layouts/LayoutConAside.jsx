b83ebb928c2c6574ce0db128f4fe7ea7d8351d80
Frany28 <franciely.vivas@gmail.com>
2025-07-30 11:58:43 -0400

    Cambios en el margin

--- Frontend/src/components/Layouts/LayoutConAside.jsx
+++ Frontend/src/components/Layouts/LayoutConAside.jsx
@@ -5,7 +5,7 @@ export default function LayoutConAside() {
   return (
     <div className="flex w-full min-h-screen">
       <AsideArchivo />
-      <div className="flex-1 bg-gray-900 overflow-auto mt-16 ml-0 lg:ml-5 p-4">
+      <div className="flex-1 bg-gray-900 overflow-auto mt-18 ml-0 lg:ml-5 lg:mt-10 p-4">
         <Outlet />
       </div>
     </div>
