bdf6d5d34e31612271aaf7885ed5f6508b68817d
Frany28 <franciely.vivas@gmail.com>
2025-08-08 16:46:36 -0400

    cambios de prueba

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,10 +3,19 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { visualizer } from "rollup-plugin-visualizer";
+import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
   root: ".",
-  plugins: [react(), tailwindcss(), visualizer({ filename: "stats.html" })],
+  plugins: [
+    react(),
+    tailwindcss(),
+    visualizer({ filename: "stats.html" }),
+    legacy({
+      targets: ["defaults", "not IE 11"], // Ajusta según compatibilidad que quieras
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"], // útil para async/await
+    }),
+  ],
   css: {
     postcss: "./postcss.config.js",
   },
