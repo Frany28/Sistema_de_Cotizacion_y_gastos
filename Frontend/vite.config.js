d76e67011aaec4468f62e5fbfde7bba81a91ef3d
Frany28 <franciely.vivas@gmail.com>
2025-08-08 16:34:51 -0400

    cambios en frontend

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,12 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   root: ".",
-  plugins: [react(), tailwindcss(), visualizer({ filename: "stats.html" })],
+  plugins: [
+    react(),
+    legacy({
+      targets: ["iOS >= 12", "safari >= 12"],
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+      renderLegacyChunks: true,
+    }),
+    tailwindcss(),
+    visualizer({ filename: "stats.html" }),
+  ],
   css: {
     postcss: "./postcss.config.js",
   },
@@ -17,6 +27,7 @@ export default defineConfig({
   build: {
     outDir: "dist",
     emptyOutDir: true,
+    target: ["es2015"],
   },
   resolve: {
     alias: {
