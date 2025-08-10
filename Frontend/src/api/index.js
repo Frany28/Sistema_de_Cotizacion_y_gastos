f88a1de10c758cd0f6e48dc382cf214db4ef36c4
Frany28 <franciely.vivas@gmail.com>
2025-08-10 14:47:32 -0400

    cambios en el export api

--- Frontend/src/api/index.js
+++ Frontend/src/api/index.js
@@ -1,7 +1,21 @@
+// Frontend/src/api/index.js
 import axios from "axios";
 
-export const api = axios.create({
+export const clienteHttp = axios.create({
   baseURL: "/api",
   withCredentials: true,
   timeout: 20000,
 });
+
+// helpers opcionales con nombre
+export const get = (ruta, opciones) => clienteHttp.get(ruta, opciones);
+export const post = (ruta, data, opciones) =>
+  clienteHttp.post(ruta, data, opciones);
+export const put = (ruta, data, opciones) =>
+  clienteHttp.put(ruta, data, opciones);
+export const patch = (ruta, data, opciones) =>
+  clienteHttp.patch(ruta, data, opciones);
+export const del = (ruta, opciones) => clienteHttp.delete(ruta, opciones);
+
+// 👇 Exportación por defecto (soluciona el error del build)
+export default clienteHttp;
