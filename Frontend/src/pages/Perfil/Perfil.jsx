59585850ebd4e7ec829bee74fde67663254a6afe
Frany28 <franciely.vivas@gmail.com>
2025-09-09 17:10:13 -0400

    perfil orden modificado

--- Frontend/src/pages/Perfil/Perfil.jsx
+++ Frontend/src/pages/Perfil/Perfil.jsx
@@ -6,20 +6,20 @@ import TarjetaPerfil from "../../components/ComponentePerfil/TarjetaPerfil";
 
 function Perfil() {
   return (
-    <div className="p-6 space-y-6">
+    <div className="p-4 space-y-4 max-w-6xl mx-auto">
       {/* Fila superior */}
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 justify-items-center">
         {/* Almacenamiento ocupa 2/3 y perfil 1/3 */}
-        <div className="md:col-span-2">
+        <div className="md:col-span-2 w-full">
           <AlmacenamientoUtilizado />
         </div>
-        <div className="md:col-span-1">
+        <div className="md:col-span-1 w-full">
           <TarjetaPerfil />
         </div>
       </div>
 
       {/* Fila inferior */}
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 justify-items-center">
         <GraficoArchivosPorTipo />
         <ArchivosRecientes />
         <EstadisticasAlmacenamiento />
