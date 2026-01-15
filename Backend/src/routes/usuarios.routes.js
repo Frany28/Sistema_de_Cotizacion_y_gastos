02f0c6ada883ee1f327ca62976ad14099f8f1140
Frany28 <franciely.vivas@gmail.com>
2026-01-14 21:58:16 -0400

    Refactor usuarios.routes.js: remove cuota validation middleware from user creation and update routes for streamlined processing.

--- Backend/src/routes/usuarios.routes.js
+++ Backend/src/routes/usuarios.routes.js
@@ -2,9 +2,7 @@
 import express from "express";
 import db from "../config/database.js";
 import { autenticarUsuario } from "../Middleware/autenticarUsuario.js";
-import validarCuota from "../Middleware/validarCuota.js"; 
 import { uploadFirma } from "../utils/s3.js";
-
 import {
   obtenerUsuarios,
   obtenerUsuarioPorId,
@@ -21,20 +19,11 @@ router.get("/", autenticarUsuario, obtenerUsuarios);
 router.get("/permisos/:permiso", autenticarUsuario);
 router.get("/:id", autenticarUsuario, obtenerUsuarioPorId);
 
-// ✅ Crear usuario (firma opcional) -> validar cuota ANTES de subir
-router.post(
-  "/",
-  autenticarUsuario,
-  validarCuota,
-  uploadFirma.single("firma"),
-  crearUsuario
-);
+router.post("/", autenticarUsuario, uploadFirma.single("firma"), crearUsuario);
 
-// ✅ Actualizar usuario (firma opcional) -> validar cuota ANTES de subir
 router.put(
   "/:id",
   autenticarUsuario,
-  validarCuota,
   uploadFirma.single("firma"),
   actualizarUsuario
 );
